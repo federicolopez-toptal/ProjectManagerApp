63f2d06c445e820d11f09408d0167e9994f6082b
Federico Lopez <gatolab@federicos-imac.local>
2019-03-15 15:05:01 -0300

    - Standard form behavior added to BaseViewController

--- ATKearneyApp/ViewControllers/BaseViewController.swift
+++ ATKearneyApp/ViewControllers/BaseViewController.swift
@@ -10,14 +10,75 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
+    private var formMode = false
+    private var scrollViewBottomConstraint: NSLayoutConstraint?
+    
     // MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    // MARK: - Interaction with UITextfields
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    
+    
+    // MARK: - Form behavior stuff
+    func addFormBehavior(scrollview: UIScrollView, bottomContraint: NSLayoutConstraint) {
+        formMode = true
+        scrollViewBottomConstraint = bottomContraint
+        scrollview.contentInsetAdjustmentBehavior = .never
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(viewOnTap(sender:)))
+        scrollview.subviews.first?.addGestureRecognizer(gesture)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if(formMode){
+            addKeyboardObservers()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if(formMode){
+            removeKeyboardObservers()
+        }
+    }
+    
+    // MARK: - Gesture(s)
+    @objc func viewOnTap(sender: UITapGestureRecognizer) {
         self.view.endEditing(true)
+    }
+    
+    // MARK: - Keyboard management
+    func addKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardEvent(n:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardEvent(n:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardEvent(n: Notification) {
+        let H = getKeyboardHeight(fromNotification: n)
+        
+        if(n.name==UIResponder.keyboardWillShowNotification){
+            scrollViewBottomConstraint!.constant = H
+        } else if(n.name==UIResponder.keyboardWillHideNotification) {
+            scrollViewBottomConstraint!.constant = 0
+        }
+        
+        view.layoutIfNeeded()
+    }
+    
+    func getKeyboardHeight(fromNotification notification: Notification) -> CGFloat {
+        if let H = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height {
+            return H
+        } else {
+            return 300
+        }
     }
     
 }
--- ATKearneyApp/ViewControllers/LoginViewController.swift
+++ ATKearneyApp/ViewControllers/LoginViewController.swift
@@ -10,48 +10,59 @@ import UIKit
 
 class LoginViewController: BaseViewController {
 
-    @IBOutlet weak var loading: UIActivityIndicatorView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     // MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
-        loading.startAnimating()
+        scrollView.subviews.first!.backgroundColor = UIColor.white
+        addFormBehavior(scrollview: scrollView, bottomContraint: bottomConstraint)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        /*
         FirebaseManager.shared.autoLogin{ (error) in
+            
             if(error == nil && CurrentUser.shared.authenticated) {
-                self.loading.stopAnimating()
-                CurrentUser.shared.trace()
-                self.performSegue(withIdentifier: "gotoProjects", sender: self)
-            } else {
-                //ADD: ALERT error auth
-            }
-        }
-        */
- 
-        
-        
-        // Standard login
-        let email = "carlos.lopez@toptal.com"
-        let pass = "gato123"
-        FirebaseManager.shared.login(email: email, password: pass) { (user, error) in
-            if(error==nil) {
-                self.loading.stopAnimating()
                 CurrentUser.shared.trace()
                 self.performSegue(withIdentifier: "gotoProjects", sender: self)
             }
         }
         
-        
-        
-        
+        //FirebaseManager.shared.logout()
     }
-        
- 
+    
+    // misc
+    func validateForm() -> Bool {
+        if(!userTextField.text!.isEmpty && !passwordTextField.text!.isEmpty) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // MARK: - Button actions
+    @IBAction func loginButtonTap(_ sender: UIButton) {
+        if(validateForm()) {
+            FirebaseManager.shared.login(email: userTextField.text!, password: passwordTextField.text!) { (user, error) in
+                if(error==nil) {
+                    CurrentUser.shared.trace()
+                    self.performSegue(withIdentifier: "gotoProjects", sender: self)
+                }
+            }
+        }
+    }
+    
+    @IBAction func forgotPassButtonTap(_ sender: UIButton) {
+    }
+    @IBAction func createAccountButtonTap(_ sender: UIButton) {
+    }
+    
     
     
     
